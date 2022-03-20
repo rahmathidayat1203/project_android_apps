1c0a68089d1d4e4ba92d202422a5a3f8840542c7
CodeBug.co <rh63800@gmail.com>
2022-03-20 18:46:56 +0700

    chage style

--- app/src/main/java/com/codebug/businessnotes/regisTerdata.kt
+++ app/src/main/java/com/codebug/businessnotes/regisTerdata.kt
@@ -3,9 +3,9 @@ package com.codebug.businessnotes
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class registerData : AppCompatActivity() {
+class regisTerdata : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_register_data)
+        setContentView(R.layout.activity_regis_terdata)
     }
 }
